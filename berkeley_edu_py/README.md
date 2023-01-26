4b5e4e0d707464bba861af98f9ac664bea65980d
LionMara <lionofmara@gmail.com>
2023-01-26 19:09:51 +0300

    Well, let's add more

--- berkeley_edu_py/README.md
+++ berkeley_edu_py/README.md
@@ -1,4 +1,5 @@
 ## This tutorial is based on berkeley.edu online notes.
  The Python Numerical Methods.
+ https://pythonnumericalmethods.berkeley.edu/
 
 # Strictly used for learning purposes.
---
+++ berkeley_edu_py/oop_tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import matplotlib.pyplot as plt
+
+
+'''
+The following is a definition of the class Point
+ that takes in the coordinate x, y.
+ Add a method plot_point that plot the position of point.
+'''
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def plot_point(self):
+        pass
+    
